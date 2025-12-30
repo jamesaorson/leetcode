accb50ac4247fb75db475666f93caf9ceaa8ef57
James Orson <jamesaorson@gmail.com>
2025-12-30 15:45:52 -0800

    Finish problem 5
    Signed-off-by: James Orson <jamesaorson@gmail.com>

---
+++ problems/5/main.py
@@ -0,0 +1,20 @@
+import os
+
+from solution import Solution
+
+PROBLEM = os.path.basename(__file__)
+
+
+if __name__ == "__main__":
+    print(f"[Problem {PROBLEM}] START")
+    assert (
+        Solution().longestPalindrome(
+            "babad",
+        ) in ["bab", "aba"]
+    )
+    assert (
+        Solution().longestPalindrome(
+            "cbbd",
+        ) in ["bb"]
+    )
+    print(f"[Problem {PROBLEM}] PASS")
---
+++ problems/5/solution.py
@@ -0,0 +1,26 @@
+class Solution:
+    def longestPalindrome(self, s: str) -> str:
+        s_len = len(s)
+        start = 0
+        result = ""
+        while start < s_len:
+            end = s_len - 1
+            while end >= start:
+                s_temp = s[start:end + 1]
+                if self.is_palindrome(s_temp):
+                    result = s_temp if len(s_temp) > len(result) else result
+                end -= 1
+            start += 1
+        return result
+
+
+    def is_palindrome(self, s: str) -> bool:
+        start = 0
+        end = len(s) - 1
+        while start < end:
+            if s[start] != s[end]:
+                return False
+            start += 1
+            end -= 1
+        return True
+
