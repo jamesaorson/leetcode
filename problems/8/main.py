a9c0c615ad906139ee5218105d1043cb739c7c33
James Orson <jamesaorson@gmail.com>
2025-12-30 16:39:51 -0800

    Finish 8
    Signed-off-by: James Orson <jamesaorson@gmail.com>

---
+++ problems/8/main.py
@@ -0,0 +1,35 @@
+import os
+
+from solution import Solution
+
+PROBLEM = os.path.basename(__file__)
+
+
+if __name__ == "__main__":
+    print(f"[Problem {PROBLEM}] START")
+    assert (
+        Solution().myAtoi(
+            "42",
+        ) == 42
+    )
+    assert (
+        Solution().myAtoi(
+            "-042",
+        ) == -42
+    )
+    assert (
+        Solution().myAtoi(
+            "1337c0d3"
+        ) == 1337
+    )
+    assert (
+        Solution().myAtoi(
+            "0-1"
+        ) == 0
+    )
+    assert (
+        Solution().myAtoi(
+            "words and 987"
+        ) == 0
+    )
+    print(f"[Problem {PROBLEM}] PASS")
---
+++ problems/8/solution.py
@@ -0,0 +1,22 @@
+class Solution:
+    def myAtoi(self, s: str) -> int:
+        if len(s) == 0:
+            return 0
+        s = s.strip()
+        sign = 1 if s[0] != '-' else -1
+        result = 0
+        if s[0] in ['-', '+']:
+            s = s[1:]
+        nums = []
+        for c in s:
+            if not c.isnumeric():
+                break
+            if c == '0':
+                continue
+            x = ord(c) - ord('0')
+            nums.append(x)
+        place = 10 ** (len(nums) - 1)
+        for num in nums:
+            result += num * place
+            place //= 10
+        return sign * result
