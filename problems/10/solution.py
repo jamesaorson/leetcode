147490420387650097afe6efe9bc0525e93fd718
James Orson <jamesaorson@gmail.com>
2025-12-30 16:52:33 -0800

    Finish 10
    Signed-off-by: James Orson <jamesaorson@gmail.com>

--- problems/10/solution.py
+++ problems/10/solution.py
@@ -1,3 +1,19 @@
 class Solution:
     def isMatch(self, s: str, p: str) -> bool:
-        pass
+        s_index = 0
+        prev_c = ''
+        for c in p:
+            if s_index >= len(s):
+                return False
+            if c == '.':
+                s_index += 1
+            elif c == '*':
+                while s_index < len(s):
+                    if prev_c == '.' or s[s_index] == prev_c:
+                        s_index += 1
+            elif c == s[s_index]:
+                s_index += 1
+            else:
+                return False
+            prev_c = c
+        return s_index == len(s)
