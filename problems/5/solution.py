5f1d8da3ac3e53b1d23b5f8b5ea7f6ccdae1d99d
James Orson <jamesaorson@gmail.com>
2026-01-01 11:46:25 -0800

    Fix longest palindromic substring
    Signed-off-by: James Orson <jamesaorson@gmail.com>

--- problems/5/solution.py
+++ problems/5/solution.py
@@ -1,27 +1,29 @@
-# TODO: Fix
 class Solution:
     def longestPalindrome(self, s: str) -> str:
-        s_len = len(s)
-        start = 0
-        result = ""
-        while start < s_len:
-            end = s_len - 1
-            while end >= start:
-                s_temp = s[start:end + 1]
-                if self.is_palindrome(s_temp):
-                    result = s_temp if len(s_temp) > len(result) else result
-                end -= 1
-            start += 1
-        return result
+        n = len(s)
+        dp = [[False] * n for _ in range(n)]
 
-
-    def is_palindrome(self, s: str) -> bool:
         start = 0
-        end = len(s) - 1
-        while start < end:
-            if s[start] != s[end]:
-                return False
-            start += 1
-            end -= 1
-        return True
+        max_len = 1
+
+        for i in range(n):
+            dp[i][i] = True
+
+        for i in range(n - 1):
+            if s[i] == s[i + 1]:
+                dp[i][i + 1] = True
+                if max_len == 1:
+                    start = i
+                    max_len = 2
+
+        for length in range(3, n + 1):
+            for i in range(n - length + 1):
+                j = i + length - 1
+                if s[i] == s[j] and dp[i + 1][j - 1]:
+                    dp[i][j] = True
+                    if length > max_len:
+                        start = i
+                        max_len = length
+        
+        return s[start:start + max_len]
 
